0f61340c4e5f287fd80bd6d6aa1238bc906a855f
“Sakyy” <sakydgny@gmail.com>
2025-06-15 13:31:58 +0700

    feat: login+darkMode

--- resources/views/livewire/auth/page/login.blade.php
+++ resources/views/livewire/auth/page/login.blade.php
@@ -1,4 +1,4 @@
-<div class="h-100 w-90 flex flex-col bg-white gap-5">
+<div class="h-100 w-90 flex flex-col gap-5">
     <div class="flex items-center gap-2">
         <img src="{{ asset('assets/images/logo/logo.png') }}" class="h-12 w-12" >
         <h1 class="text-3xl font-semibold">HRMS</h1>
@@ -8,19 +8,31 @@
         <p class="text-sm text-grayColor">Silahkan login untuk melanjutkan</p>
     </div>
     <form class="flex flex-col gap-3">
-        <label for="" class="flex flex-col py-1 px-2 border-2 border-primary rounded-lg">
-            <p class="text-xs font-semibold text-primary">Email Address</p>
-            <input type="text" class="w-full focus:outline-none">
+        <label for="email" 
+        class="flex flex-col-reverse py-1 px-2 border-2 border-grayColor rounded-lg focus-within:border-primary ">
+            <input 
+            type="email" 
+            id="email" 
+            class="w-full focus:outline-none peer"
+            >
+            <p class="text-xs font-semibold text-grayColor peer-focus:text-primary">Email Address</p>
         </label>
-        <label x-data="{ show: false }" class="flex flex-row justify-between items-center py-1 px-2 border-2 border-primary rounded-lg">
-            <div class="flex-1">
-                <p class="text-xs font-semibold text-primary">Password</p>
-                <input :type="show ? 'text' : 'password'" class="w-full focus:outline-none">
+
+        <label 
+        for="password" 
+        x-data="{ showPw: true}" 
+        class="flex flex-row justify-between items-center py-1 px-2 border-2 border-grayColor focus-within:border-primary rounded-lg"
+        >
+            <div  class="flex flex-col-reverse ">
+                <input  
+                x-bind:type="showPw ? 'text' : 'password'" id="password" 
+                class="w-full focus:outline-none peer">
+                <p class="text-xs font-semibold text-grayColor peer-focus:text-primary">Password</p>
             </div>
             <i 
-                :class="show ? 'fa-regular fa-eye' : 'fa-regular fa-eye-slash'" 
-                class="text-primary cursor-pointer ml-2"
-                @click="show = !show"
+            x-bind:class="showPw ? 'fa-solid fa-eye' : 'fa-solid fa-eye-slash'"
+            class=" text-primary cursor-pointer ml-2"
+            x-on:click="showPw = !showPw"
             >
             </i>
         </label>
