78466c8ae2ad91e97c36822d58b7a05b67de145a
Putra <alphaaldiansyah@gmail.com>
2025-06-16 09:29:34 +0700

    feat : add dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Livewire\AllEmployee;
+use App\Livewire\Dashboard;
+use App\Livewire\Header;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
-use App\Livewire\Testbang;
+use App\Livewire\Test;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/dashboard', Dashboard::class);
 
+Route::get('/all-employee', AllEmployee::class);
